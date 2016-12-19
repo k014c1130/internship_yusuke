e971d971cc345a2e54e086af36562d2cae59c43a
ITユーザー <it-user@kpc-it3152mc.kmt.neec.ac.jp>
2016-12-19 13:23:16 +0900

    Completed. #3

---
+++ ex1-1-3/ex1-1-3/ViewController.m
@@ -0,0 +1,93 @@
+//
+//  ViewController.m
+//  ex1-1-3
+//
+//  Created by ITユーザー on 2016/12/19.
+//  Copyright © 2016年 ITユーザー. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    //分岐用変数
+    NSUInteger a = 1;
+    
+    //高速列挙構文の配列
+    NSArray *ar = [NSArray arrayWithObjects:@"東京", @"名古屋", @"大阪", nil];
+  
+
+    
+    //if文
+    if(a == 1){
+        NSLog(@"if文:a = 1 の場合(ここでa = 2に変更)");
+        a = 2;
+    }
+    
+    //if〜else文
+    if(a == 1){
+        NSLog(@"if〜else文:a = 1 の場合");
+    }else{
+        NSLog(@"if〜else文:a = 1 以外の場合");
+    }
+    
+    //if〜else if文
+    if(a == 1){
+        NSLog(@"else if文:a = 1 の場合");
+    }else if(a == 2){
+        NSLog(@"else if文:a = 2 の場合");
+    }
+    
+    //三項演算子
+    a == 1 ? NSLog(@"三項演算子:真の場合") : NSLog(@"三項演算子:偽の場合");
+    
+    //for文
+    for(NSInteger i = 1;i <= 3;i++){
+        NSLog(@"for文:%ld回目の実行",i);
+    }
+    
+    //高速列挙構文
+    
+    for(NSString *str in ar){
+        NSLog(@"%@",str);
+    }
+    
+    //switch文
+    switch (a) {
+        case 1:
+            NSLog(@"switch文: a = 1の場合");
+            break;
+            
+        case 2:
+            NSLog(@"switch文: a = 2の場合");
+            break;
+            
+        case 3:
+            NSLog(@"switch文: a = 3の場合");
+            break;
+            
+        default:
+            NSLog(@"switch: default分岐");
+            break;
+    }
+    
+    
+    
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
